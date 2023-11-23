ea357c1374ff8d56de026b705e975290bbeb92f2
mrozowski <mrozowskion@gmail.com>
2023-11-24 04:02:40 +0700

    Initial

---
+++ seat-reservation/src/main/java/com/mrozowski/seatreservation/SeatReservationApplication.java
@@ -0,0 +1,13 @@
+package com.mrozowski.seatreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatReservationApplication.class, args);
+	}
+
+}
---
+++ seat-reservation/src/test/java/com/mrozowski/seatreservation/SeatReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrozowski.seatreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
